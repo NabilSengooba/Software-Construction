0d5e9ff526826d53ec8b3229aaa32a42653aac3d
NabilSengooba <nabilbil482@gmai.com>
2024-02-05 08:29:57 +0300

    Added code for rearranging

---
+++ A97227/rearrange_1.py
@@ -0,0 +1,13 @@
+import re
+
+def rearrange_name(name):
+    # Use regular expression to match the expected name format
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+
+    # Check if the name matches the expected format
+    if result is None:
+        # If not, return the original name
+        return name
+
+    # Rearrange the name in the desired format (last name, first name)
+    return "{} {}".format(result[2], result[1])
